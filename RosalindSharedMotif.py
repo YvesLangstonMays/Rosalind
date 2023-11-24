9bcf19c654e5ff3c4e8e35d6658e657480fbad2b
YvesLangstonMays <yveslangstonmays@gmail.com>
2023-11-24 09:24:10 -0600

    Updates

--- RosalindSharedMotif.py
+++ RosalindSharedMotif.py
@@ -1,18 +1,10 @@
-currentLine = None
 seqString = ""
 seqDict = {}
 with open("SharedMotif.txt", "r") as SM:
-    sequence = ""
-    sequenceCount = 0
     for line in SM:
+        print(line)
         line = line.split("\n")
         line = "".join(line)
-        if line[0] == ">":
-            currentLine = line
-            if not seqDict:
-                pass
-            else:
-                seqDict[currentLine] = seqString
-        else:
-            seqString += line
+
+
 print(seqDict)
