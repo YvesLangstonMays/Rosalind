7ec5595ace5369805475db83cd0f478014ecaeae
YvesLangstonMays <yveslangstonmays@gmail.com>
2023-11-25 19:54:51 -0600

    Shared

--- RosalindSharedMotif.py
+++ RosalindSharedMotif.py
@@ -1,10 +1,26 @@
 seqString = ""
 seqDict = {}
-with open("SharedMotif.txt", "r") as SM:
-    for line in SM:
-        print(line)
-        line = line.split("\n")
-        line = "".join(line)
+
+title = None
+current = []
 
 
-print(seqDict)
+def FASTA_open(file):
+    with open(file, "r") as f:
+        for line in f:
+            if line[0] == ">":
+                if len(current) > 0:
+                    seqDict[title] = ["".join(item) for item in current]
+                    current.clear()
+                title = line.replace("\n", "")
+                title = "".join(title)
+            else:
+                line = line.replace("\n", "")
+                current.append(line)
+        seqDict[title] = [item for item in current]
+        current.clear()
+
+
+FASTA_open("SharedMotif.txt")
+for item in seqDict:
+    print(seqDict[item])
